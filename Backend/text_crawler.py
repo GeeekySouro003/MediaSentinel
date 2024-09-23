148299089516bf6be1612175f0b9c8edd688124a
Souradip <dgsouro77@gmail.com>
2024-09-23 11:10:35 +0530

    Added the scrape function and add some missing imports

--- Backend/text_crawler.py
+++ Backend/text_crawler.py
@@ -27,7 +27,30 @@ async def scrape_ndtv_archive(news_source:str,selected_date:str,source_url:str,m
                         
                         
                     }
+                    
+                    for index,link in enumerate(article_links.find_all("a",href = True),start=1) 
+                    
+                    if (
+                        
+                        (domain_match := re.search(r'https?://(?:www\.)?([^/]+'),link["href"])
+                        and domain_match.group(1) == "ndtv.com" and "india-news" in link["href"]
+                    )
+                ][:max_articles_to_scrape]
+                
+                
+                tasks= [
+                    for index,article in enumerate(article_data,start=1)
                 ]
+                    
+                
+                await asyncio.gather(*tasks)
+                
+                with open() as json_file:                 ##pass in parameters the english.json from frontend
+                    json.dump(article_data,json_file,ensure_ascii=False,indent=4)
+            
+            
+            else:
+                print(f'[Error] Failed to retrieve data from {source_url}\nStatus code: {response.status}')
                 
                 
                 
