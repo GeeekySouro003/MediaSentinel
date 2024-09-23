9e19c468e7fa56bef9703bbce3f2ca4da8fc0bec
Souradip <dgsouro77@gmail.com>
2024-09-23 08:16:18 +0530

    Added the scraping function

--- Backend/app.py
+++ Backend/app.py
@@ -5,3 +5,17 @@ from flask import Flask,request,jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
+
+
+@app.route("/api", methods = ["POST"])
+
+def process_data() -> str:
+    news_source=request.json("news_source")
+    selected_date=request.json("selected_date")
+    
+    if news_source == "NDTV":
+        source_url=f"https://archives.ndtv.com/articles/{selected_date}.html"
+        max_articles_to_scrape=500
+
+if __name__ == "__main__":
+    app.run()
