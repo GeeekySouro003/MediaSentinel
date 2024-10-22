c322133c2f346ab119648c56ed1b47a03094b23a
Souradip <dgsouro77@gmail.com>
2024-10-22 13:23:10 +0530

    Added the sentiment analyzer using BERT

---
+++ Backend/sentiment_analyzer.py
@@ -0,0 +1,26 @@
+import torch
+from transformers import BertTokenizer, BertForSequenceClassification
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+##trained_model = BertForSequenceClassification.from_pretrained(r'Backend\Models\Sentiment')
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+trained_model.to(device)
+
+def analyze_sentiment(text: str) -> str:
+    tokenized_text = tokenizer(text, truncation=True, padding=True, return_tensors="pt")
+
+    encoded_text = {
+        key: val.to(device)
+        for key, val in tokenized_text.items()
+    }
+
+    with torch.inference_mode():
+        predicted_scores = trained_model(**encoded_text).logits
+
+    sentiment_index = predicted_scores.argmax(dim=1).item()
+    sentiment_labels = ["Positive", "Neutral", "Negative"]
+    predicted_sentiment = sentiment_labels[sentiment_index]
+
+    return predicted_sentiment
